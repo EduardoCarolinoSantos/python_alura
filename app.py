43263d212ea22b8570b980b6183d3e74df38516e
Eduardo Carolino Santos <eduardocarolinosantos2510@gmail.com>
2026-02-18 11:02:57 -0300

    Aprendendo python do 0

---
+++ app.py
@@ -0,0 +1,10 @@
+print ("Sabor Express\n")
+
+print('1 . Cadastrar restaurantes')
+print('2 . Listar restaurantes')
+print('3 . Ativar restaurantes')
+print('4 . Sair\n')
+
+# Pegar informações dos usuários = Comando input.
+
+opcao_escolhida = input('Escolha uma opção: ')
