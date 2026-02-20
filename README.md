8d5e4e68dc5f0d92f2b9df7566ddb8adda4730b0
Eduardo Carolino Santos <eduardocarolinosantos2510@gmail.com>
2026-02-20 19:03:45 -0300

    Módulo 1 (Aula 1 à 5 - Primeiros passos com Python.)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python_alura
+
+## Módulo 1️⃣
+
+- Aprendo a formatar prints e as convenções iniciais da linguagem. 
+- Criação de variáveis e captação de dados do usuário.
--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-print ("Sabor Express\n")
+# Por convenção da linguagem Python utiliza aspas simples para strings, mas também é possível utilizar aspas duplas. O importante é manter a consistência no código.
+
+print ("""
+       
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+       """)
 
 print('1 . Cadastrar restaurantes')
 print('2 . Listar restaurantes')
@@ -7,4 +17,6 @@ print('4 . Sair\n')
 
 # Pegar informações dos usuários = Comando input.
 
+# No python os nomes das variáveis utilizam o padrão snake_case, ou seja, letras minúsculas e palavras separadas por underline.
 opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
